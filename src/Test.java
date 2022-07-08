e9f7a7602c44824be896079f6369c13602b3a2fd
yigit <yigitkazan@hotmail.com>
2022-07-07 21:51:56 -0400

    Its is important.

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5,6,7,7,8,9};
+
+        for(int i=0; i<array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+
+    }
+
+
+}
